21833b845ea758c5236a8646ad319a73f9a1c1ff
Michael <michael.hrabe@gmail.com>
2024-01-01 23:13:15 +0100

    test

--- src/main.py
+++ src/main.py
@@ -1,5 +1,10 @@
+from datetime import date
+
 import arrr
 from pyscript import document
+from js import updateChart
+
+from pyodide.ffi import create_proxy, to_js
 
 
 def translate_english(event):
@@ -7,3 +12,6 @@ def translate_english(event):
     english = input_text.value
     output_div = document.querySelector("#output")
     output_div.innerText = arrr.translate(english)
+    updateChart(to_js([
+        date(2022,6,6), date(2023,4,7), date(2023,7,7), date(2023,7,10)]
+    ), to_js([1,5,2,4]))
